3bf65dbc0cda0de69f1cd8c2ef1194e130f551a5
mh511q <abo7mayd511@gmail.com>
2025-08-06 08:57:22 +0300

    2.2

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// firebase-config.js
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDAAmbFDN8DPCLTx1pP0YSI4IIP9qWgulI",
+  authDomain: "baqaa-hospital.firebaseapp.com",
+  databaseURL: "https://baqaa-hospital-default-rtdb.firebaseio.com",
+  projectId: "baqaa-hospital",
+  storageBucket: "baqaa-hospital.firebasestorage.app",
+  messagingSenderId: "1069887196926",
+  appId: "1:1069887196926:web:aa03cfe1fe134a31599faf",
+  measurementId: "G-9WDL06CC3G"
+};
+
+// تهيئة Firebase (باستخدام صيغة الإصدار 8)
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database(); // الحصول على مرجع لقاعدة البيانات
